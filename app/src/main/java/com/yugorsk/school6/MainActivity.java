e8a95832eb6da6d301eb5c4eb1e130088e4a13da
Antoha1397 <toscha.17@mail.ru>
2019-08-28 23:25:26 +0500

    Добавил навигацию

--- app/src/main/java/com/yugorsk/school6/MainActivity.java
+++ app/src/main/java/com/yugorsk/school6/MainActivity.java
@@ -1,14 +1,98 @@
 package com.yugorsk.school6;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+
+import com.google.android.material.navigation.NavigationView;
+import com.yugorsk.school6.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    NavController navController;
+    ActivityMainBinding binding;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setupNavigation();
+    }
+
+    private void setupNavigation() {
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Главная");
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.app_name, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = findViewById(R.id.navigationView);
+
+        navController = Navigation.findNavController(this, R.id.hostFragment);
+
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        menuItem.setChecked(true);
+
+        drawerLayout.closeDrawers();
+
+        int id = menuItem.getItemId();
+
+        switch (id) {
+
+            case R.id.Main:
+                navController.navigate(R.id.Main);
+               toolbar.setTitle("Главная");
+                break;
+
+            case R.id.AboutSchool:
+                navController.navigate(R.id.AboutSchool);
+                toolbar.setTitle("О школе");
+                break;
+                
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
